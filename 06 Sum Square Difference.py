40c71737558e3e509ef8378831aea86e04535919
alijamieson <music@alijamieson.co.uk>
2026-05-26 19:39:40 +0100

    tue

--- 06 Sum Square Difference.py
+++ 06 Sum Square Difference.py
@@ -5,3 +5,26 @@
 # Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385.
 
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+target = 100
+
+def sumofsq():
+    global a
+    a=0
+    for i in range(target+1):
+        a+=(i**2)
+    # print(a)
+
+
+def sqofsum():
+    global d
+    d=0
+    for j in range(target+1):
+        d+=j
+    # print(d**2)
+
+sqofsum()
+sumofsq()
+
+print((d**2)-a)
+
