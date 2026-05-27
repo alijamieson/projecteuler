260aa633f475fc92dd591b39a9055b13dbe8b5a8
alijamieson <music@alijamieson.co.uk>
2026-05-27 14:32:29 +0100

    wed

---
+++ 11 Largest Product in a Grid.py
@@ -0,0 +1,5 @@
+# In the 20 x 20 grid below, four numbers along a diagonal line have been marked in red (https://projecteuler.net/problem=11)
+
+# The product of these numbers is 26 x 63 x 73 x 14 = 1788696
+
+# What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20 x 20 grid?
