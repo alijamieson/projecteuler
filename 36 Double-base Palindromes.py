140cb31d1bcb9d3ccfcc461b15a5314b501616b8
alijamieson <music@alijamieson.co.uk>
2026-08-16 19:39:15 +0100

    test

--- 36 Double-base Palindromes.py
+++ 36 Double-base Palindromes.py
@@ -1,5 +1,11 @@
 # The decimal number 585 = 1001001001 in binary is palindromic in both bases.
-
 # Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2
-
 # (Please note that the palindromic number, in either base, may not include leading zeros.)
+# https://www.w3schools.com/python/ref_func_bin.asp
+# https://www.w3schools.com/python/python_howto_reverse_string.asp
+
+b=[]
+def palmdne(a):
+    global b
+    if str(a)==reversed(str(a)):
+        b.append(a)
