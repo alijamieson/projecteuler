d2fceb5bc366965b422167f460d87169e38b0dc0
alijamieson <music@alijamieson.co.uk>
2026-08-27 10:56:16 +0100

    Update 03 Largest Prime Factor.py

--- 03 Largest Prime Factor.py
+++ 03 Largest Prime Factor.py
@@ -1,1 +1,18 @@
 # What is the largest prime factor of the number 600851475143?
+
+n = 600851475143
+
+import math 
+def prime_factors(num):
+  factors = []
+  factor = 2
+
+  while (num >= 2):
+    if (num % factor == 0):
+      factors.append(factor)
+      num = num / factor
+    else:
+      factor += 1
+  return factors
+
+print(max(prime_factors(n)))
